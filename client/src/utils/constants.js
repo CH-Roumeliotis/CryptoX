2e593058af3fe84e10210fbc04e79503aed02337
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2022-04-09 18:26:48 +0300

    SC Done

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x63EB895e9580F5f3671832220B5aBBA4F0AbF06b";
+export const contractABI = abi.abi;
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-ropsten.alchemyapi.io/v2/TAiGT6jQ9-gBMOnQ56CZf_TQhFGjuWgz
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle');
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.5',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/TAiGT6jQ9-gBMOnQ56CZf_TQhFGjuWgz',
+      accounts: ['f7263e782ed4aeec893052cb53469df47f2ab8a586339863c2b59276ade49dcf']
+    }
+  }
+}
