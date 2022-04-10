52003b1e40aa3c259cd5795113c427132191e7b2
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2022-04-11 00:01:08 +0300

    final

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0x63EB895e9580F5f3671832220B5aBBA4F0AbF06b";
+export const contractAddress = "0xAd7a43B22375Bf3309A199c34C74030E60F719DC";
 export const contractABI = abi.abi;
